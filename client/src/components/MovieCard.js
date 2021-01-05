b770b84b1f57cef055ee3a0c7d27344d372073a5
Rudy <hactiv8rud@gmail.com>
2021-01-05 13:16:53 +0700

    added no-image svg

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -17,7 +17,13 @@ class MovieCard extends React.Component {
       <>
         <div className="col mt-2">
           <div className="card">
-            <img src={this.state.image_url} className="card-img-top" alt={this.props.movie.original_title} />
+            {
+              (this.props.movie.poster_path) ? (
+                <img src={this.state.image_url} className="card-img-top" alt={this.props.movie.original_title} />
+              ) : (
+                <img src="/images/no-image.svg" className="card-img-top" alt={this.props.movie.original_title} />
+              )
+            }
             <div className="card-body d-flex flex-column text-center">
               <h5 className="card-title">{this.props.movie.original_title}</h5>
               <a href="" onClick={this.goDetails} className="btn btn-success">See Details</a>
